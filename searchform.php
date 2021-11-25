05d6a2e65378fefe65435d360cbd2bad100b91ab
Tajeshwar <tkhara@gmail.com>
2021-11-25 17:25:06 +0530

    Traditional wordpress search setup complete when js is not available. Custom search logic is not implemented for the traditional search.

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form class="search-form" method="GET" action="<?php echo  esc_url( site_url( "/" ) );  ?>">
+  <label for="s" class="headline headline--medium">Perform a new search</label>
+  <div class="search-form-row">
+    <input class="s" type="search" name="s" id="s" placeholder="What are you looking for?">
+    <input class="search-submit" type="submit" value="Search">
+  </div>
+</form>
--- single-skill.php
+++ single-skill.php
@@ -49,6 +49,49 @@
             get_template_part( 'template-parts/content-project');
           }
           }
+
+          
+          // *** Related clients start
+          // We are trying to pull in clients that have this related skill
+
+          // Remember to reset if there is a query above
+          wp_reset_postdata();
+          
+          // Find clients and filter for the current skill
+          // Skill on this page
+          $homepageClients = new WP_Query(array(
+            'posts_per_page' => 10,
+            // What are we looking for? Clients
+            'post_type' => 'client',
+            'order' => 'ASC',
+            'meta_query' => array(
+              // If the array of related skills contains
+              // the id of the current skill
+              array(
+                'key' => 'related_skills',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
+              )
+            )
+          ));
+
+          // Loop through the results
+
+          // Are there any results?
+          if($homepageClients->have_posts()) {
+            // Headline
+          echo "<hr class='section-break'>";
+          echo "<h2 class='headline headline--medium'>Related Clients</h2>";
+          echo '<ul class="professor-cards">';
+          // Yes we have results loop through them  
+          while($homepageClients->have_posts()) {
+            $homepageClients->the_post(); 
+            // Get relevant template from template-parts
+            get_template_part( 'template-parts/content', get_post_type(  ) );
+          } // while end
+          echo '</ul>';
+          }
+          // End
         ?>
 
 </div>
