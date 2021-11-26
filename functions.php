fed444af90517af8aa2cf7d21471c0f51f44b829
Tajeshwar <tkhara@gmail.com>
2021-11-26 21:21:02 +0530

    Added a filter in functions.php to make all note posts private.

--- functions.php
+++ functions.php
@@ -182,3 +182,15 @@ add_filter('login_headertitle', 'ourLoginTitle');
 function ourLoginTitle() {
   return get_bloginfo('name');
 }
+
+// Force note posts to be private
+add_filter( 'wp_insert_post_data', 'makeNotePrivate');
+
+function makeNotePrivate($data) {
+
+  if($data["post_type"] == "note" && $data["post_status"] != "trash") {
+    $data['post_status'] = "private";
+  }
+  
+  return $data;
+}
